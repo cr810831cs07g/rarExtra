b738655e6e9dd214ecc53cd63bd57bd62d7db981
cr810831cs07g <49565154+cr810831cs07g@users.noreply.github.com>
2022-01-19 09:18:29 +0800

    Update and rename rarExtraTest.py to rarExtraCCSCAN.py

---
+++ rarExtraCCSCAN.py
@@ -0,0 +1,29 @@
+# -*- coding: utf8 -*-
+from importlib.metadata import files
+import unrar,os, sys, glob
+from rarfile import RarFile
+
+pwd1 = 'P@ssw0rd'           # 第一層解壓密碼
+pwd2 = '[Corecloud]'        # 第二層解壓密碼
+path1 = os.path.dirname(os.path.realpath(__file__))
+for i in glob.glob("*.rar"):
+    os.chdir(path1)
+    with RarFile(i, 'r') as myrar:
+        myrar.extract('m.rar.log', path='tmp', pwd=pwd1)
+        myrar.close()
+
+        path2 = './tmp'
+        file = 'm.rar.log'
+        os.chdir(path2)
+        new_file = i[:-4] + '_' + file[:-4]
+        print(file)
+        print(new_file)
+        os.rename(file, new_file)
+'''
+# dangerous 
+os.chdir(path2)
+for s in glob.glob("*.rar"):
+    with RarFile(s, 'r') as danrar:
+        danrar.extractall(path=s, pwd=pwd2)
+        dan.close()
+'''
